daa1ede0cb0a9bb9cd075dc0408317f83e80bcf6
pveloso <pedrovelosofernandes@outlook.com>
2025-12-03 19:21:25 +0000

    feat(users): configure UserAdmin for custom user model

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,1 +1,67 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    """
+    Custom admin for User model with email authentication and username display.
+    """
+
+    # Fields to display in the admin list view
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+        "date_joined",
+    )
+    list_filter = ("is_staff", "is_superuser", "is_active", "date_joined")
+    search_fields = ("username", "email", "first_name", "last_name")
+    ordering = ("-date_joined",)
+
+    # Fieldsets for the admin detail/edit view
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("username", "first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    # Fieldsets for adding a new user
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "username",
+                    "password1",
+                    "password2",
+                    "first_name",
+                    "last_name",
+                    "is_staff",
+                    "is_active",
+                ),
+            },
+        ),
+    )
+
+    readonly_fields = ("last_login", "date_joined")
