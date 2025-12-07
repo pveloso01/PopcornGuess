65346e9fd539e2bcde653cfc0de6e5f764b3b7b4
pveloso <pedrovelosofernandes@outlook.com>
2025-12-07 19:37:57 +0000

    feat: add login, register, and social auth endpoints

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -15,4 +15,9 @@ router.register(r"users", UserViewSet, basename="user")
 
 urlpatterns = [
     path("", include(router.urls)),
+    # dj-rest-auth authentication endpoints
+    path("auth/", include("dj_rest_auth.urls")),  # login, logout, password reset, etc.
+    path(
+        "auth/registration/", include("dj_rest_auth.registration.urls")
+    ),  # registration
 ]
